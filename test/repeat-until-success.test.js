5f84cd20bd0ebcaa4168fcca822bcad810a555b3
François Michaud <fmichaud@9fevrier.com>
2018-01-29 15:13:30 +0100

    Refactor code

--- test/repeat-until-success.test.js
+++ test/repeat-until-success.test.js
@@ -2,7 +2,7 @@ const should = require('should'); // eslint-disable-line no-unused-vars
 
 const bunyan = require('bunyan');
 
-const repeatUntilSuccess = require('../lib/repeat-until-success');
+const {repeatUntilSuccess} = require('../lib/repeat-until-success');
 
 
 /**
@@ -47,26 +47,26 @@ describe('Base unit testing for [Function=repeatUntilSuccess]', () => {
 
   it('should throw an error if the function does not return a promise', () => {
     (() => {
-      repeatUntilSuccess(this.invalidFn, args=[2]);
+      repeatUntilSuccess(this.invalidFn, args=[2]).run();
     }).should.throw('Function must be a promise');
   });
 
   it('should throw an error if delay is not an integer', () => {
     (() => {
-      repeatUntilSuccess(this.simpleFn, args=[2], delay='string');
+      repeatUntilSuccess(this.simpleFn, args=[2]).delay('string');
     }).should.throw('Delay must be an integer');
   });
 
   it('should throw an error if timeout is not an integer', () => {
     (() => {
-      repeatUntilSuccess(this.simpleFn, args=[2], delay=5000, timeout='string');
+      repeatUntilSuccess(this.simpleFn, args=[2]).timeout('string');
     }).should.throw('Timeout must be an integer');
   });
 
   it('should throw an error if provided logger is not a logger', () => {
     (() => {
-      repeatUntilSuccess(this.simpleFn, args=[2], delay=5000, timeout=-1, logger=this.invalidLogger);
-    }).should.throw('Provided logger does not seem a logger');
+      repeatUntilSuccess(this.simpleFn, args=[2]).logger(this.invalidLogger);
+    }).should.throw('NotALoggerError: Provided logger does not contain a (method: debug)');
   });
 });
 
@@ -75,6 +75,6 @@ describe('Base unit testing for [Function=repeatUntilSuccess]', () => {
  */
 describe('Running repeatUntilSuccess', () => {
   it('should be resolved', () => {
-      repeatUntilSuccess(this.fn, null, 100, timeout=-1).should.be.resolved();
+      repeatUntilSuccess(this.fn).delay(100).run().should.be.resolved();
   });
 });
